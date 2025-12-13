c23c152eb5ad0bd536f67d43b03c01463cd9fec2
TommyTNeruo <tommytkroberts@gmail.com>
2025-12-13 19:28:42 +0000

    Created a path scouting function for beh files

--- analysis/src/main.py
+++ analysis/src/main.py
@@ -2,8 +2,10 @@ import polars as pl
 from pathlib import Path
 
 def scout_files():
-
-    
+    root_dir = Path(__file__).parents[2]
+    sub_dir = root_dir / 'data' / 'raw'
+    beh_data_paths = sorted(sub_dir.rglob('*/*/beh/*.csv'))
+    return beh_data_paths
 
 if __name__ == "__main__":
-    main()
+    scout_files()
